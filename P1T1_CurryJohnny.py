01d64b9e890bc839928b9ae86000de02e27b7393
Lenkie12 <66932476+Lenkie12@users.noreply.github.com>
2020-06-22 14:01:16 -0400

    Add files via upload

---
+++ P1T1_CurryJohnny.py
@@ -0,0 +1,9 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('This is my IDLE Test.')
+This is my IDLE Test.
+>>> print('I wish to learn more about Python and how to use it...')
+I wish to learn more about Python and how to use it...
+>>> print('Effectively and efficiently!!!')
+Effectively and efficiently!!!
+>>> 
---
+++ P2HW1_BasicMath_JohnnyCurry.py
@@ -0,0 +1,26 @@
+#To get the sum and product of two numbers.
+#22 June 2020
+#CTI-110 P2HW1 - Basic Math
+#Johnny Curry
+#
+
+#Get the first number.
+first_number = float(input('Enter the first number: '))
+
+# Get the second number.
+second_number = float(input('Enter the second number: '))
+
+#Add the two numbers.
+total_add = first_number + second_number
+
+#Display the sum.
+print('The sum is ', int(total_add))
+
+#Multiply the two numbers.
+total_multiply = first_number * second_number
+
+#Display the product.
+print('The product is ', int(total_multiply))
+
+
+
